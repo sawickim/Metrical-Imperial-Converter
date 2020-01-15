39b5e2cdd74bb82904e2dbcf68e8e7d317cfe763
Marek Sawicki <msawickimail@gmail.com>
2020-01-15 10:49:22 +0100

    after changes

--- src/main/java/com/example/converter/MetricToImperialConverter.java
+++ src/main/java/com/example/converter/MetricToImperialConverter.java
@@ -2,28 +2,28 @@ package com.example.converter;
 
 public class MetricToImperialConverter {
 
-    public static double metersToFoot(String meter) {
-        double foot = 3.2808 *  Double.parseDouble(meter);
+    public static double metersToFoot(double meter) {
+        double foot = 3.2808 *  meter;
         return foot;
     }
 
-    public static double centymetrsToInch(String centymeters) {
-        double inch = 0.3937 *  Double.parseDouble(centymeters);
+    public static double centymetrsToInch(double centymeters) {
+        double inch = 0.3937 *  centymeters;
         return inch;
     }
 
-    public static double litersToGallons(String liters) {
-        double gallons = 1.7597 *  Double.parseDouble(liters);
+    public static double litersToGallons(double liters) {
+        double gallons = 1.7597 *  liters;
         return gallons;
     }
 
-    public static double kilogramsToPounds(String kilograms) {
-        double pounds = 2.2046 *  Double.parseDouble(kilograms);
+    public static double kilogramsToPounds(double kilograms) {
+        double pounds = 2.2046 *  kilograms;
         return pounds;
     }
 
-    public static double celsiusToFaranhait(String celsius) {
-        double faranhait = 1.8 * Double.parseDouble(celsius) + 32;
+    public static double celsiusToFaranhait(double celsius) {
+        double faranhait = 1.8 * celsius + 32;
         return faranhait;
     }
 }
--- src/test/java/com/example/converter/ConverterTestImperialToMetrical.java
+++ src/test/java/com/example/converter/ConverterTestImperialToMetrical.java
@@ -5,133 +5,164 @@ import org.junit.jupiter.api.Assertions;
 
 public class ConverterTestImperialToMetrical {
 
+    private boolean compare(double value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value is out of range");
+        }
+        return Double.compare(value,0) > 0;
+    }
+
     //feet to meters
     @Test
     public void zeroFeetTestFeetToMeters() {
-        Assertions.assertEquals(0, ImperialToMetricalConverter.footToMeters("0"));
+        Assertions.assertEquals(0, ImperialToMetricalConverter.footToMeters(0));
     }
 
     @Test
     public void oneFeetTestFeetToMeters() {
-        Assertions.assertEquals(0.3048, ImperialToMetricalConverter.footToMeters("1"));
+        Assertions.assertEquals(0.3048, ImperialToMetricalConverter.footToMeters(1));
     }
 
     @Test
     public void oneFeetNotEquilsZeroTestFeetToMeters() {
-        Assertions.assertNotEquals(0, ImperialToMetricalConverter.footToMeters("1"));
+        Assertions.assertNotEquals(0, ImperialToMetricalConverter.footToMeters(1));
     }
 
     @Test
     public void twoFeetTestFeetToMeters() {
-        Assertions.assertEquals(0.6096, ImperialToMetricalConverter.footToMeters("2"));
+        Assertions.assertEquals(0.6096, ImperialToMetricalConverter.footToMeters(2));
     }
 
     @Test
     public void twoFeetsNotEquilsCorrectTestFeetToMeters() {
-        Assertions.assertNotEquals(6, ImperialToMetricalConverter.footToMeters("2"));
+        Assertions.assertNotEquals(6, ImperialToMetricalConverter.footToMeters(2));
+    }
+
+    @Test
+    public void minusOneFeetsTestFeetToMeters(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                compare(ImperialToMetricalConverter.footToMeters(-1)));
     }
 
     //inch to centymeters
     @Test
     public void zeroInchTestInchsToCentymeters() {
-        Assertions.assertEquals(0, ImperialToMetricalConverter.inchToCentymeters("0"));
+        Assertions.assertEquals(0, ImperialToMetricalConverter.inchToCentymeters(0));
     }
 
     @Test
     public void oneInchsTestInchToCentymeters() {
-        Assertions.assertEquals(2.54, ImperialToMetricalConverter.inchToCentymeters("1"));
+        Assertions.assertEquals(2.54, ImperialToMetricalConverter.inchToCentymeters(1));
     }
 
     @Test
     public void oneInchsNotEquilsZeroTestInchToCentymeters() {
-        Assertions.assertNotEquals(0, ImperialToMetricalConverter.inchToCentymeters("1"));
+        Assertions.assertNotEquals(0, ImperialToMetricalConverter.inchToCentymeters(1));
     }
 
     @Test
     public void twoInchsTestInchToCentymeters() {
-        Assertions.assertEquals(5.08, ImperialToMetricalConverter.inchToCentymeters("2"));
+        Assertions.assertEquals(5.08, ImperialToMetricalConverter.inchToCentymeters(2));
     }
 
     @Test
     public void twoInchsNotEquilsCorrectTestInchToCentymeters() {
-        Assertions.assertNotEquals(6, ImperialToMetricalConverter.inchToCentymeters("2"));
+        Assertions.assertNotEquals(6, ImperialToMetricalConverter.inchToCentymeters(2));
+    }
+
+    @Test
+    public void minusOneInchsTestInchToCentymeters(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                compare(ImperialToMetricalConverter.inchToCentymeters(-1)));
     }
 
     //gallons to liters
     @Test
     public void zeroGallonsTestGallonsToLiters() {
-        Assertions.assertEquals(0, ImperialToMetricalConverter.gallonsToLiters("0"));
+        Assertions.assertEquals(0, ImperialToMetricalConverter.gallonsToLiters(0));
     }
 
     @Test
     public void oneGallonsTestGallonsToLiters() {
-        Assertions.assertEquals(3.7854, ImperialToMetricalConverter.gallonsToLiters("1"));
+        Assertions.assertEquals(3.7854, ImperialToMetricalConverter.gallonsToLiters(1));
     }
 
     @Test
     public void oneGallonsNotEquilsZeroTestGallonsToLiters() {
-        Assertions.assertNotEquals(0, ImperialToMetricalConverter.gallonsToLiters("1"));
+        Assertions.assertNotEquals(0, ImperialToMetricalConverter.gallonsToLiters(1));
     }
 
     @Test
     public void twoGallonsTestGallonsToLiters() {
-        Assertions.assertEquals(7.5708,ImperialToMetricalConverter.gallonsToLiters("2"));
+        Assertions.assertEquals(7.5708,ImperialToMetricalConverter.gallonsToLiters(2));
     }
 
     @Test
     public void twoGallonsNotEquilsCorrectTestGallonsToLiters() {
-        Assertions.assertNotEquals(6, ImperialToMetricalConverter.inchToCentymeters("2"));
+        Assertions.assertNotEquals(6, ImperialToMetricalConverter.gallonsToLiters(2));
+    }
+
+    @Test
+    public void minusOneGallonsTestGallonsToLiters(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                compare(ImperialToMetricalConverter.gallonsToLiters(-1)));
     }
 
     //pounds to kilograms
     @Test
     public void zeroPoundsTestPoundsToKilograms() {
-        Assertions.assertEquals(0, ImperialToMetricalConverter.poundsToKilograms("0"));
+        Assertions.assertEquals(0, ImperialToMetricalConverter.poundsToKilograms(0));
     }
 
     @Test
     public void onePoundsTestPoundsToKilograms() {
-        Assertions.assertEquals(0.4535, ImperialToMetricalConverter.poundsToKilograms("1"));
+        Assertions.assertEquals(0.4535, ImperialToMetricalConverter.poundsToKilograms(1));
     }
 
     @Test
     public void onePoundsNotEquilsZeroTestPoundsToKilograms() {
-        Assertions.assertNotEquals(0, ImperialToMetricalConverter.poundsToKilograms("1"));
+        Assertions.assertNotEquals(0, ImperialToMetricalConverter.poundsToKilograms(1));
     }
 
     @Test
     public void twoPoundsTestPoundsToKilograms() {
-        Assertions.assertEquals(0.907,ImperialToMetricalConverter.poundsToKilograms("2"));
+        Assertions.assertEquals(0.907,ImperialToMetricalConverter.poundsToKilograms(2));
     }
 
     @Test
     public void twoPoundsNotEquilsCorrectTestPoundsToKilograms() {
-        Assertions.assertNotEquals(6, ImperialToMetricalConverter.inchToCentymeters("2"));
+        Assertions.assertNotEquals(6, ImperialToMetricalConverter.poundsToKilograms(2));
+    }
+
+    @Test
+    public void minusOnePoundTestPoundsToKilograms(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                compare(ImperialToMetricalConverter.poundsToKilograms(-1)));
     }
 
     //Faranhaite to Celius
     @Test
     public void zeroFaranhaitesTestFaranhaitresToCelsiuses() {
-        Assertions.assertEquals(-17.77777777777778, ImperialToMetricalConverter.faranhaitToCelsius("0"));
+        Assertions.assertEquals(-17.77777777777778, ImperialToMetricalConverter.faranhaitToCelsius(0));
     }
 
     @Test
     public void oneFaranhaitesTestFaranhaitresToCelsiuses() {
-        Assertions.assertEquals(-17.22222222222222, ImperialToMetricalConverter.faranhaitToCelsius("1"));
+        Assertions.assertEquals(-17.22222222222222, ImperialToMetricalConverter.faranhaitToCelsius(1));
     }
 
     @Test
     public void oneFaranhaitesNotEquilsZeroTestFaranhaitresToCelsiuses() {
-        Assertions.assertNotEquals(0, ImperialToMetricalConverter.faranhaitToCelsius("1"));
+        Assertions.assertNotEquals(0, ImperialToMetricalConverter.faranhaitToCelsius(1));
     }
 
     @Test
     public void twoFaranhaitesTestFaranhaitresToCelsiuses() {
-        Assertions.assertEquals(-16.666666666666668,ImperialToMetricalConverter.faranhaitToCelsius("2"));
+        Assertions.assertEquals(-16.666666666666668,ImperialToMetricalConverter.faranhaitToCelsius(2));
     }
 
     @Test
     public void twoFaranhaitesNotEquilsCorrectTestFaranhaitresToCelsiuses() {
-        Assertions.assertNotEquals(-15, ImperialToMetricalConverter.faranhaitToCelsius("2"));
+        Assertions.assertNotEquals(-15, ImperialToMetricalConverter.faranhaitToCelsius(2));
     }
 }
